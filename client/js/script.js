b6178017cf063a1ad8c903299e8b24d1e9aad194
Sean <seanreilly123@hotmail.com>
2020-04-01 02:53:52 +1300

    Cards on home done. Profile page works

--- client/js/script.js
+++ client/js/script.js
@@ -1,22 +1,26 @@
-console.log("Hello");
-let url, fullName;
+let url, fullName, productId;
 const STARTING_BALANCE = 100; // starting balance for all registerers
 
 $(document).ready(function(){
 	// check log in and hide and show everything
 	$('#navLoggedIn').hide();
 	$('#registerForm').hide();
-
+	$("#productCards").show();
+	
 	if (sessionStorage['username']) {
-	$('#navLoggedIn').show();
-	$('#navLoggedOut').hide();
-	fullName = sessionStorage.getItem('userFName') + " " + sessionStorage.getItem('userLName');
-	document.getElementById("firstNameGreeting").innerHTML = sessionStorage.getItem('userFName');
+		$('#navLoggedIn').show();
+		$('#navLoggedOut').hide();
+		fullName = sessionStorage.getItem('userFName') + " " + sessionStorage.getItem('userLName');
+		document.getElementById("firstNameGreeting").innerHTML = sessionStorage.getItem('userFName');
 	} 
 	else {
-	$('#navLoggedIn').hide();
-	$('#navLoggedOut').show();
+		$('#navLoggedIn').hide();
+		$('#navLoggedOut').show();
 	}
+
+	$(".navbar-brand").click(function(){
+		showAllProducts();
+	});
 
 	//get url and port from config.json
 	$.ajax({
@@ -25,18 +29,47 @@ $(document).ready(function(){
 		dataType :'json',
 		success : function(data){
 			url = `${data.SERVER_URL}:${data.SERVER_PORT}`;
+			showAllProducts();
+			addProfileDetails();
+			// showProductInfo();
 		},//success
-			error:function(){
+		error:function(){
 			console.log('error: cannot call api');
 		}//error
 	});//ajax
+
+	//Load cards
+	function showAllProducts(){
+		$.ajax({
+			url: `${url}/products`,
+			type: 'GET',
+			dataType :'json',
+			success: function(data){
+				console.log(data);
+				if(window.location.href.includes("index.html")){
+					document.getElementById('productCards').innerHTML = "";
+					for (let i = 0; i < data.length; i++) {
+						document.getElementById('productCards').innerHTML +=
+						`<a href="product.html" class="product-link" id="${data[i]["_id"]}"><div class="card col">
+						<img class="img-thumbnail" src="${data[i].image}" alt="Image">
+						<div class="card-body">
+						<h3 class="card-title"> ${data[i].title}</h3>
+						<h4 class="card-text">$${data[i].price}</h4>
+						</div></div></a>`;
+					}
+				}
+			},
+			error: function(error) {
+				console.log('no good');
+			}
+		})
+	}
 
 	//login
 	$('#loginButton').click(function(){
 		let username = $('#loginUsername').val();
 		let password = $('#loginPassword').val();
-		console.log(username, password);
-	
+
 		if (username == '' || password == ''){
 			alert('Please enter all details');
 		} 
@@ -70,6 +103,8 @@ $(document).ready(function(){
 						$('#navLoggedIn').show();
 						$('#navLoggedOut').hide();
 						$('#registerForm').hide();
+						console.log(window.location.href)
+						showAllProducts();
 					}
 				},
 				error:function(){
@@ -80,16 +115,18 @@ $(document).ready(function(){
 	});//login form
 
 	// logout button
-	$('#logoutButton').click(function(){
+	$('.logoutButton').click(function(){
 		sessionStorage.clear();
 		$('#navLoggedIn').hide();
 		$('#navLoggedOut').show();
 		$('#loginUsername').val("");
 		$('#loginPassword').val("");
+		$("#productCards").show();
 	});
 
 	// show register
 	$('#registerButton').click(function(){
+		$("#productCards").hide();
 		$('#registerUsername').val('');
 		$('#registerFirstName').val('');
 		$('#registerLastName').val('');
@@ -107,7 +144,6 @@ $(document).ready(function(){
 		let location = $('#registerLocation').val();
 		let email = $('#registerEmail').val();
 		let password = $('#registerPassword').val();
-		console.log(username,fname,lname,location,email, password);
 
 		if (username == '' ||fname == '' || lname == '' || location == '' || email == '' || password == ''){
 			alert('Please enter all details');
@@ -134,11 +170,13 @@ $(document).ready(function(){
 					}
 					else{
 						$('#registerForm').hide();
+						$('#loginUsername').focus();
 						swal({
 							title: 'Success!',
-							text: `New account registered ${username}`,
+							text: `Congratulations! Your new account has been registered\n Please log in to continue`,
 							icon: 'success',
 							button: 'Okay!',
+							timer: 2500
 						});
 					}
 				},
@@ -149,5 +187,59 @@ $(document).ready(function(){
 		} // else
 	});//register form
 
+	// add profile details on account page
+	function addProfileDetails(){
+		$.ajax({
+			url :`${url}/users/u=${sessionStorage.getItem('userID')}`,
+			type :'GET',
+			dataType :'json',
+			success : function(data){
+				console.log(data);
+				if(window.location.href.includes("profile.html")){
+					$("#profile-username").html(data.username);
+					$("#profile-fullname").html(fullName);
+					$("#profile-email").html(data.email);
+					$("#profile-balance").html(`$${data.balance}`);
+				}
+			},//success
+			error:function(){
+				console.log('error: cannot call api');
+			}//error
+		});//ajax
+	}// add profile details
+
+	// $(".product-link").click(function(){
+	// 	sessionStorage.setItem('productId',"this id");
+	// 	productId = $(this).attr('id');
+	// 	// console.log($(this).id, productId, sessionStorage)	
+	// })
+
+	// function showProductInfo(){
+	// 	let id = sessionStorage.getItem('productId');
+	// 	console.log(productId);
+	// 	$.ajax({
+	// 		url :`${url}/products/`,
+	// 		type :'GET',
+	// 		dataType :'json',
+	// 		success : function(data){
+	// 			console.log(data);
+	// 			if(window.location.href.includes("product.html")){
+	// 				$("#product-info").html(data.title)
+	// 			}
+	// 		},//success
+	// 		error:function(){
+	// 			console.log('error: cannot call api');
+	// 		}//error
+	// 	});//ajax
+	// }
+
+	// add and remove active list class on profile side bar
+	$(".account-info__sidebar__list-item").click(function(){
+		let list = document.querySelectorAll(".account-info__sidebar__list-item");
+		for(var i = 0; i < list.length; i++){
+			list[i].classList.remove("account-info__sidebar__list-item--active");
+		}
+		$(this).addClass("account-info__sidebar__list-item--active")
+	});
 }); // document
 
