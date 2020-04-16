e64da16e5e96e52a4a4c8acc8d852f06973c762b
Sean <seanreilly123@hotmail.com>
2020-04-17 11:12:08 +1200

    Production minification complete

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Summative 3
+
 This is our summative 3 project. This is the front end and backend project.
 You can run this through postman or on the local host
 
 ## Setup
+
 Clone into a www folder and npm i the file. Change config-copy to config and enter the credentials and you're good to go
+
+## Enjoy
--- backend/Gruntfile.js
+++ backend/Gruntfile.js
@@ -1,40 +1,39 @@
-module.exports = function(grunt) {
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'index.js',
-        dest: 'index.min.js'
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['./models/*.js', 'index.js'],
-        tasks: ['jshint'],
-        options: {
-          interrupt: false,
-        }
-      },
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'index.js', './models/*.js'],
-      options: {
-        esversion: 6
-      }
-    }
-  });
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        uglify: {
+            options: {
+                banner:
+                    '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+            },
+            build: {
+                src: "index.js",
+                dest: "index.min.js",
+            },
+        },
+        watch: {
+            scripts: {
+                files: ["./models/*.js", "index.js"],
+                tasks: ["jshint"],
+                options: {
+                    interrupt: false,
+                },
+            },
+        },
+        jshint: {
+            all: ["Gruntfile.js", "index.js", "./models/*.js"],
+            options: {
+                esversion: 6,
+            },
+        },
+    });
 
-  // Load the plugins that provides tasks
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint'); // watch
+    // Load the plugins that provides tasks
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-uglify-es");
+    grunt.loadNpmTasks("grunt-contrib-jshint"); // watch
 
-
-  // Default task(s).
-  grunt.registerTask('default', ['watch']);
-
+    // Default task(s).
+    grunt.registerTask("default", ["watch"]);
 };
